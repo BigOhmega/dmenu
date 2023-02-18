7b45e366d9ef09d522c806bfdf1dfc6cc18997d3
BigOhmega <antgit@tuta.io>
2023-02-18 17:16:06 -0500

    editted border width, lines, and overall width

--- config.h
+++ config.h
@@ -14,10 +14,10 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static const unsigned int border_width = 5;
+static unsigned int lines      = 10;
+static const unsigned int border_width = 2;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 1250;                    /* minimum width when centered */
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
